fae8dd314c03d794b57b106b963c46ffa5bfb2b6
Jack Kendrick <jackkendrick@Jacks-MacBook-Pro.local>
2023-04-25 18:28:31 -0500

    added a seeds.sql file to pre-populate the employee database

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+USE employee_db;
+
+INSERT INTO department (id, department_name)
+VALUES (001, "Algebra I", "Linear equations, inequalities, functions, and graphs", true),
+       (002, "Pre-Calculus", "Polynomials, Complex Numbers, Vectors", true),
+       (003, "Calculus I", "Limits, Differentiation, Derivatives", true),
+       (004, "Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
+       
