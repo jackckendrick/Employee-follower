cdcb33e23f5002051602ed86075bb22c1f2fa36e
Jack Kendrick <jackkendrick@Jacks-MacBook-Pro.local>
2023-05-02 17:43:45 -0500

    added arrays for the functions

--- index.js
+++ index.js
@@ -1,174 +1,233 @@
-const inquirer = require ('inquirer');
-const fs = require ('fs');
-const mysql = require ('mysql2');
-const { type } = require('os');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const mysql = require("mysql2");
+const { type } = require("os");
 // require("console.table")
 
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: 'Dawn1kevin',
-    database: 'employee_db'
-})
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "Dawn1kevin",
+  database: "employee_db",
+});
 
 function firstQuestion() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: ['View all employees', 'Add employee', 'Update employee role', 'View all roles', 'Add a role', 'View all departments', 'Quit'],
-            name: 'question_1'
-        },
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "Add employee",
+          "Update employee role",
+          "View all roles",
+          "Add a role",
+          "View all departments",
+          "Add department",
+          "Quit",
+        ],
+        name: "question_1",
+      },
     ])
-    .then(answer => {
-        if(answer.question_1 === "View all employees"){
-            viewEmployees()
-        } 
-        else if(answer.question_1 === "Add employee"){
-            addEmployee()
-        }
-        else if(answer.question_1 === "Update employee role"){
-            updateEmployeeRole()
-        }
-        else if(answer.question_1 === "View all roles"){
-            viewRoles()
-        }
-        else if(answer.question_1 === "Add a role"){
-            addRole()
-        }
-        else if(answer.question_1 === "View all departments"){
-            viewDepartments()
-        }
-        else {
-            quit()
-        }
-    })
+    .then((answer) => {
+      if (answer.question_1 === "View all employees") {
+        viewEmployees();
+      } else if (answer.question_1 === "Add employee") {
+        addEmployee();
+      } else if (answer.question_1 === "Update employee role") {
+        updateEmployeeRole();
+      } else if (answer.question_1 === "View all roles") {
+        viewRoles();
+      } else if (answer.question_1 === "Add a role") {
+        addRole();
+      } else if (answer.question_1 === "View all departments") {
+        viewDepartments();
+      } else if (answer.question_1 === "Add department") {
+        addDepartment();
+      } else {
+        quit();
+      }
+    });
 }
 
 function viewEmployees() {
-    db.query("SELECT * FROM employee", (err, results) =>{
-        if(err) {
-            console.log(err)
-        } else {
-            console.table(results)
-        }
-    } )
+  db.promise()
+    .query("SELECT * FROM employee")
+    .then(([rows]) => {
+      console.table(rows);
+      firstQuestion();
+    })
+    .catch((err) => console.log(err));
 }
 
 function addEmployee() {
-    inquirer.prompt([
-       {
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the employee's first name?",
-        name: "first_name"
-       },
-       {
+        name: "first_name",
+      },
+      {
         type: "input",
         message: "What is the employee's last name?",
-        name: "last_name" 
-       },
-       {
+        name: "last_name",
+      },
+      {
         type: "list",
         message: "What is the employee's role id?",
-        choices: ["L1", "L2", "L3", "Counsel", "Paralegal", "Janitor", "Security", "Accountant 1", "Accountant 2", "CFO", "Account Executive", "Sales Representative"],
-        name: "role_id"
-       },
-       {
+        choices: [
+          "L1",
+          "L2",
+          "L3",
+          "Counsel",
+          "Paralegal",
+          "Janitor",
+          "Security",
+          "Accountant 1",
+          "Accountant 2",
+          "CFO",
+          "Account Executive",
+          "Sales Representative",
+        ],
+        name: "role_id",
+      },
+      {
         type: "list",
         message: "Who is the employee's manager?",
         choices: ["Anne", "Tom", "Kathy", "Kenya", "Josh", "John"],
-        name: "manager_id"
-
-       },
+        name: "manager_id",
+      },
     ])
-    .then(answers => {
-        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.first_name}", "${answers.last_name}", ${answers.role_id}, ${answers.manager_id});`, (err, results) => {
-            if(err) {
-                console.log(err)
-            } else {
-                console.log("Employee has been added!")
-            }
-        })
-    })
+    .then((answers) => {
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.first_name}", "${answers.last_name}", ${answers.role_id}, ${answers.manager_id});`,
+        (err, results) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Employee has been added!");
+          }
+        }
+      );
+    });
+  firstQuestion();
 }
 
 function updateEmployeeRole() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "What is the employee's name?",
-            name: "first_name"
-        },
-        {
-            type: "input",
-            message: "What is the employee's new role id?",
-            name: "role_id"
-        }
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employee's name?",
+        name: "first_name",
+      },
+      {
+        type: "input",
+        message: "What is the employee's new role id?",
+        name: "role_id",
+      },
     ])
-    .then(answers => {
-        db.query(`UPDATE employee SET role_id = ${answers.role_id} WHERE first_name =${answers.first_name}`, (err, results) => {
-            if(err) {
-                console.log(err)
-            } else {
-                console.log("Employee has been updated!")
-            }
-        })
+    .then((answers) => {
+      db.query(
+        `UPDATE employee SET role_id = ${answers.role_id} WHERE first_name =${answers.first_name}`,
+        (err, results) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Employee has been updated!");
+          }
+        }
+      );
+    });
+  firstQuestion();
+}
+
+function viewRoles() {
+  db.promise()
+    .query(
+      "SELECT role.id, role.title AS Title, role.salary AS Salary, department.department_name AS Department FROM role LEFT JOIN department ON role.department_id = department.id"
+    )
+    .then(([rows]) => {
+      console.table(rows);
+      firstQuestion();
     })
+    .catch((err) => console.log(err));
 }
 
-function viewRoles() { 
-    db.query("SELECT * FROM role", (err, results) =>{
-    if(err) {
-        console.log(err)
-    } else {
-        console.table(results)
-    }
-} )
-}
-
-function addRole() {
-    inquirer.prompt([
-       {
+async function addRole() {
+    const [departments] = await db.promise().query("SELECT * FROM department")
+    const departmentsArray = departments.map(department => (
+        {name: department.department_name,value: department.id}
+    ))
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the role title?",
-        name: "role_title"
-       },
-       {
+        name: "role_title",
+      },
+      {
         type: "input",
         message: "What is the role's salary?",
-        name: "salary" 
-       },
-       {
-        type: "input",
+        name: "salary",
+      },
+      {
+        type: "list",
         message: "What department is the role in?",
-        name: "department"
-       }
+        name: "department",
+        choices: departmentsArray
+      },
     ])
-    .then(answers => {
-        db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${answers.role_title}", "${answers.salary}", ${answers.department});`, (err, results) => {
-            if(err) {
-                console.log(err)
-            } else {
-                console.log("Role has been added!")
-            }
-        })
-    })
+    .then((answers) => {
+      db.query(
+        `INSERT INTO role (title, salary, department_id) VALUES ("${answers.role_title}", "${answers.salary}", ${answers.department});`,
+        (err, results) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Role has been added!");
+          }
+        }
+      );
+    });
+//   firstQuestion();
 }
 
 function viewDepartments() {
-    db.query("SELECT * FROM department", (err, results) =>{
-        if(err) {
-            console.log(err)
-        } else {
-            console.table(results)
+  db.promise()
+    .query("SELECT * FROM department")
+    .then(([rows]) => {
+      console.table(rows);
+      firstQuestion();
+    })
+    .catch((err) => console.log(err));
+}
+
+function addDepartment() {
+  inquirer.prompt({
+    type: "input",
+    name: "department_name",
+    message: "Enter department name",
+  }).then(({department_name})=> {
+    db.promise().query("INSERT INTO department SET ?",{department_name})
+    .then(([rows])=> {
+        if(rows.affectedRows>0){
+            viewDepartments();
         }
-    } )
+        else{
+            console.error("Failed to add department")
+            firstQuestion();
+        }
+    })
+  })
 }
 
 function quit() {
-    console.log("Goodbye!")
-    process.exit()
+  console.log("Goodbye!");
+  process.exit();
 }
 
 firstQuestion();
--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     department_name VARCHAR(30)
 );
 
@@ -12,8 +12,8 @@ CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
-    -- FOREIGN KEY (department_id) REFERENCES department(id)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 
@@ -22,16 +22,16 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
-    -- FOREIGN KEY (manager_id) REFERENCES employee(id)
-    -- FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-INSERT INTO department (id, department_name)
-VALUES (1, "Engineering"), (2, "Legal"), (3, "Service"), (4, "Finance"), (5, "Sales");
+INSERT INTO department (department_name)
+VALUES ("Engineering"), ("Legal"), ("Service"), ("Finance"), ("Sales");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("L1", 100000, 1), ("L2", 150000, 1), ("L3", 200000, 1), ("Counsel", 210000, 2), ("paralegal", 90000, 2), ("janitor", 50000, 3), ("security", 70000, 3), ("Accountant 1", 60000, 4), ("Accountant 2", 80000, 4), ("CFO", 450000, 4), ("Account executive", 120000, 5), ("Sales representative", 85000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jack", "Kendrick", 4, 3), ("Lisa", "Thomas", 2, 7), ("Jane", "Doe", 3, 5), ("John", "Smith", 1, 5), ("Steve", "Jackson", 6, 2), ("Allison", "Perry", 6, 2), ("Jim", "Cook", 3, 1), ("Alex", "Kim", 5, 2), ("Tim,", "Roberts", 4, 4), ("Ryan", "Sartin", 3, 1), ("Tom", "Nordan", 4, 6), ("Lily", "Ho", 5, 2), ("Anne", "Strait", 3, 7), ("Kim", "Kardashian", 3, 9), ("Khloe", "Thompson", 5, 11), ("Todd", "Chrisley", 7, 3), ("Kenya", "Moore", 3, 8), ("Kandi", "Burrus", 2, 11), ("Kathy", "Hilton", 6, 12);
+VALUES ("Jack", "Kendrick", 4, null), ("Lisa", "Thomas", 2, 1), ("Jane", "Doe", 3, 1), ("John", "Smith", 1, 2), ("Steve", "Jackson", 6, 2), ("Allison", "Perry", 6, 2), ("Jim", "Cook", 3, 1), ("Alex", "Kim", 5, 2), ("Tim,", "Roberts", 4, 4), ("Ryan", "Sartin", 3, 1), ("Tom", "Nordan", 4, 6), ("Lily", "Ho", 5, 2), ("Anne", "Strait", 3, 7), ("Kim", "Kardashian", 3, 2), ("Khloe", "Thompson", 5, 3), ("Todd", "Chrisley", 7, 3), ("Kenya", "Moore", 3, 4), ("Kandi", "Burrus", 2, 2), ("Kathy", "Hilton", 6, 1);
